50c23e2dcd841accb5b768a73da5bdb460f94527
TinyRocket <michael@tinyrocket.co>
2014-10-19 15:26:18 -0700

    [+] Added the object foundation for users

---
+++ src/Tinyrocket/Toggl/Object/AbstractObject.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * This file is part of the Toggl package
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @subpackage    Tinyrocket\Toggl
+ * @version       1.0.0
+ * @author        TinyRocket <michael@tinyrocket.co>
+ * @license       BSD License (3-clause)
+ * @copyright     Copyright 2014 TinyRocket
+ * @link          http://tinyrocket.co/Toggl
+ */
+namespace Tinyrocket\Toggl\Object;
+
+/**
+ *	Toggl\Toggl\Object\AbstractObject
+ *
+ *	Primary Toggl class. Connects to all individual
+ *	resource classes
+ */
+abstract class AbstractObject {
+
+}
